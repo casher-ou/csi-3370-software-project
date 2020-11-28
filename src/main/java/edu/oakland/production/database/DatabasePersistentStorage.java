d2bedc4fe775c82ed8ac66037277420cc1f10090
Robertson Brinker <60048070+robertsonbrinker@users.noreply.github.com>
2020-11-28 17:52:23 -0500

    Update DatabasePersistentStorage.java

--- src/main/java/edu/oakland/production/database/DatabasePersistentStorage.java
+++ src/main/java/edu/oakland/production/database/DatabasePersistentStorage.java
@@ -3,38 +3,22 @@ package edu.oakland.production.database;
 import edu.oakland.helper.admin.LocationDataPoint;
 import edu.oakland.helper.admin.TrackData;
 
-public class DatabasePersistentStorage {
+public interface DatabasePersistentStorage {
 
-  public int locateRfidData() {
-    return 0;
-  }
+  public int locateRfidData() 
 
-  public LocationDataPoint getLocationDataPoint(int offset) {
-    return null;
-  }
+  public LocationDataPoint getLocationDataPoint(int offset) 
 
-  public void storeLocationDataPoint(LocationDataPoint locationDataPoint) {
+  public void storeLocationDataPoint(LocationDataPoint locationDataPoint) 
+    
+  public TrackData getTrackData(int offset) 
 
-  }
-
-  public TrackData getTrackData(int offset) {
-    return null;
-  }
-
-  public void storeTrackData(TrackData trackData) {
-
-  }
+  public void storeTrackData(TrackData trackData) 
   
-  public String getMode() {
-    return "";
-  }
+  public String getMode() 
 
-  public void storeMode(String mode) {
-
-  }
+  public void storeMode(String mode) 
   
-  public String getNextSat(String currentSat) {
-    return "";
-  }
+  public String getNextSat(String currentSat) 
 }
 
