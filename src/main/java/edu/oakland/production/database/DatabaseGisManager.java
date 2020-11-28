8e4559fcc76d26b26caa3d53b96f3162c0308fbe
Robertson Brinker <60048070+robertsonbrinker@users.noreply.github.com>
2020-11-28 17:40:19 -0500

    Update DatabaseGisManager.java

--- src/main/java/edu/oakland/production/database/DatabaseGisManager.java
+++ src/main/java/edu/oakland/production/database/DatabaseGisManager.java
@@ -2,18 +2,11 @@ package edu.oakland.production.database;
 
 import edu.oakland.helper.admin.LocationDataPoint;
 
-public class DatabaseGisManager {
+public interface DatabaseGisManager {
 
-  public void passStoreRequest(LocationDataPoint locationDataPoint) {
+  public void passStoreRequest(LocationDataPoint locationDataPoint) 
 
-  }
+  public void passModeRequest(String mode) 
 
-  public void passModeRequest(String mode) {
-
-  }
-
-  public String passNextSatRequest(String currentSat) {
-    return "";
-  }
-  
+  public String passNextSatRequest(String currentSat) 
 }
