1d1637f1268a2c70bb8561184e633ec60cadf985
robertsonbrinker <robertsonbrinker@gmail.com>
2020-11-28 20:04:45 -0500

    Create DatabaseGisManagerClass.java

---
+++ src/main/java/edu/oakland/production/database/DatabaseGisManagerClass.java
@@ -0,0 +1,18 @@
+package edu.oakland.production.database;
+
+import edu.oakland.helper.admin.LocationDataPoint;
+
+public class DatabaseGisManager {
+
+  public void passStoreRequest(LocationDataPoint locationDataPoint) {
+    
+  }
+
+  public void passModeRequest(String mode) {
+    
+  }
+
+  public String passNextSatRequest(String currentSat) {
+    return "";
+  }
+}
