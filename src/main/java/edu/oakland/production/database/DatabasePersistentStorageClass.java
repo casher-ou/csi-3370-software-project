f431090613143727966671c3eec5ee9138e7526d
MathewYaldo <55512598+MathewYaldo@users.noreply.github.com>
2020-11-29 16:54:46 -0500

    Update DatabasePersistentStorageClass.java

---
+++ src/main/java/edu/oakland/production/database/DatabasePersistentStorageClass.java
@@ -0,0 +1,43 @@
+package edu.oakland.production.database;
+
+import edu.oakland.helper.admin.LocationDataPoint;
+import edu.oakland.helper.admin.TrackData;
+
+
+public interface DatabasePersistentStorage {
+
+  public int locateRfidData(){
+  	return 0;
+  }
+
+  public LocationDataPoint getLocationDataPoint(int offset) {
+  	return null;
+  }
+
+  public void storeLocationDataPoint(LocationDataPoint locationDataPoint) {
+
+  }
+
+  public TrackData getTrackData(int offset) {
+  	return null;
+  }
+
+  public void storeTrackData(TrackData trackData) {
+
+  }
+
+  public String getMode() {
+  	return "";
+  }
+
+  public void storeMode(String mode) {
+
+  }
+
+  public String getNextSat(String currentSat) {
+  	return "";
+  }
+
+
+}
+
