e7322ba5f3ecc8fd54d42090bd4563722b460b85
kwillard34 <60444686+kwillard34@users.noreply.github.com>
2020-11-29 15:41:41 -0500

    Build-TrackDataCalculator

---
+++ src/main/java/edu/oakland/helper/middleware02/TrackDataCalculator.java
@@ -0,0 +1,15 @@
+package edu.oakland.helper.middleware02;
+
+import edu.oakland.helper.admin.LocationDataPoint;
+
+public class TrackDataCalculator {
+    static public float calculateSpeed(LocationDataPoint[] locationDataPoints){
+
+    }
+    static public float calculateDirection(LocationDataPoint[] locationDataPoints){
+
+    }
+    static private LocationDataPoint[] calculateRootMeanSquared(LocationDataPoint[] locationDataPoints){
+        
+    }
+}
