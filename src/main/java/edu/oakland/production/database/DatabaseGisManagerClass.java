e085347eeb4db6dcad8697442459172d8e17039a
robertsonbrinker <robertsonbrinker@gmail.com>
2020-11-28 20:15:22 -0500

    Update DatabaseGisManagerClass.java

--- src/main/java/edu/oakland/production/database/DatabaseGisManagerClass.java
+++ src/main/java/edu/oakland/production/database/DatabaseGisManagerClass.java
@@ -2,7 +2,7 @@ package edu.oakland.production.database;
 
 import edu.oakland.helper.admin.LocationDataPoint;
 
-public class DatabaseGisManager {
+public class DatabaseGisManagerClass {
 
   public void passStoreRequest(LocationDataPoint locationDataPoint) {
     
