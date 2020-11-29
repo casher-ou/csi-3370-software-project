3691154a5a8956fd2864f4cc2a56946188b5e86a
Jessica Cortes <jcortes@oakland.edu>
2020-11-28 19:35:29 -0500

    Create TrackDataPlotter.java

---
+++ src/main/java/edu/oakland/helper/display02/TrackDataPlotter.java
@@ -0,0 +1,24 @@
+package edu.oakland.helper.display02;
+
+public class TrackDataPlotter(){
+
+  public TrackDataPlotter(TrackData data){
+
+  }
+  public void displayPlotter(){
+    
+  }
+  private XYSeries getTrackData(){
+
+  }
+  private XYSeries calculateLine(int[] x, int[] y){
+
+  }
+  private int[] extractLatFromTrackData(TrackData data){
+
+  }
+  private int[] extractLngFromTrackData(TrackData data){
+
+  }
+
+}
