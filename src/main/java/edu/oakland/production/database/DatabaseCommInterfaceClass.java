f37b169da5aed878953e8ee13933acec926ab72d
Ian-Klaus <73550576+Ian-Klaus@users.noreply.github.com>
2020-11-29 16:27:25 -0500

    Update DatabaseCommInterfaceClass.java

--- src/main/java/edu/oakland/production/database/DatabaseCommInterfaceClass.java
+++ src/main/java/edu/oakland/production/database/DatabaseCommInterfaceClass.java
@@ -2,12 +2,28 @@ package edu.oakland.production.database;
 
 public class DatabaseCommInterfaceClass {
   
-  public DatabaseCommInterfaceClass() {
+  public DatabaseCommInterfaceClass(DatabaseCommManager databaseCommManager) {
     return null;
   }
   
-  public DatabaseCommManager databaseCommManager() {
+  public int receiveGetRfidRequest() {
+    return 0;
+  }
+  
+  public LocationDataPoint receiveGetLocationDataPointRequest(int offset) {
     return null;
+  }
+  
+  public TrackData receiveGetTrackDataRequest(int offset) {
+    return null;
+  }
+  
+  public void storeTrackDataRequest(TrackData trackData) {
+    
+  }
+  
+  public String receiveGetModeRequest() {
+    return null; 
   }
   
 }
